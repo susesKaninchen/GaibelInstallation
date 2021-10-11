c7a2eb467431bec67bbe6f1108a32e5305a94bee
Marco Gabrecht <gabrecht@tzl.de>
2021-10-11 14:45:11 +0200

    Version von letztem Treffen

---
+++ Master/Master.ino
@@ -0,0 +1,275 @@
+#include <esp_now.h>
+#include <WiFi.h>
+
+// Global copy of slave
+#define NUMSLAVES 5
+esp_now_peer_info_t slavesL[NUMSLAVES] = {};
+int SlaveCntL = 0;
+esp_now_peer_info_t slavesR[NUMSLAVES] = {};
+int SlaveCntR = 0;
+
+#define CHANNEL 0
+#define PRINTSCANRESULTS 0
+
+uint8_t data = 0;
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+const int LaserPin = 18;     // the number of the pushbutton pin
+const int LaserPin_1 = 19;     // the number of the pushbutton pin
+int besucherzahler = 0;
+#define halloFile 10
+#define maxFiles 10// exlusive diese Zahl
+#define debounceValue 1000
+
+// Init ESP Now with fallback
+void InitESPNow() {
+  WiFi.disconnect();
+  if (esp_now_init() == ESP_OK) {
+    Serial.println("ESPNow Init Success");
+  }
+  else {
+    Serial.println("ESPNow Init Failed");
+    ESP.restart();
+  }
+}
+
+// Scan for slaves in AP mode
+void ScanForSlave() {
+  int8_t scanResults = WiFi.scanNetworks();
+  //reset slaves
+  memset(slavesL, 0, sizeof(slavesL));
+  SlaveCntL = 0;
+  memset(slavesR, 0, sizeof(slavesR));
+  SlaveCntR = 0;
+  Serial.println("");
+  if (scanResults == 0) {
+    Serial.println("No WiFi devices in AP Mode found");
+  } else {
+    Serial.print("Found "); Serial.print(scanResults); Serial.println(" devices ");
+    for (int i = 0; i < scanResults; ++i) {
+      // Print SSID and RSSI for each device found
+      String SSID = WiFi.SSID(i);
+      int32_t RSSI = WiFi.RSSI(i);
+      String BSSIDstr = WiFi.BSSIDstr(i);
+
+      if (PRINTSCANRESULTS) {
+        Serial.print(i + 1); Serial.print(": "); Serial.print(SSID); Serial.print(" ["); Serial.print(BSSIDstr); Serial.print("]"); Serial.print(" ("); Serial.print(RSSI); Serial.print(")"); Serial.println("");
+      }
+      delay(10);
+      // Check if the current device starts with `Slave`
+      if (SSID.indexOf("Left") == 0) {
+        Serial.print(i + 1); Serial.print(": "); Serial.print(SSID); Serial.print(" ["); Serial.print(BSSIDstr); Serial.print("]"); Serial.print(" ("); Serial.print(RSSI); Serial.print(")"); Serial.println("");
+        int mac[6];
+        if ( 6 == sscanf(BSSIDstr.c_str(), "%x:%x:%x:%x:%x:%x",  &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5] ) ) {
+          for (int ii = 0; ii < 6; ++ii ) {
+            slavesL[SlaveCntL].peer_addr[ii] = (uint8_t) mac[ii];
+          }
+        }
+        slavesL[SlaveCntL].channel = CHANNEL; // pick a channel
+        slavesL[SlaveCntL].encrypt = 0; // no encryption
+        SlaveCntL++;
+      }
+      if (SSID.indexOf("Right") == 0) {
+        Serial.print(i + 1); Serial.print(": "); Serial.print(SSID); Serial.print(" ["); Serial.print(BSSIDstr); Serial.print("]"); Serial.print(" ("); Serial.print(RSSI); Serial.print(")"); Serial.println("");
+        int mac[6];
+        if ( 6 == sscanf(BSSIDstr.c_str(), "%x:%x:%x:%x:%x:%x",  &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5] ) ) {
+          for (int ii = 0; ii < 6; ++ii ) {
+            slavesR[SlaveCntR].peer_addr[ii] = (uint8_t) mac[ii];
+          }
+        }
+        slavesR[SlaveCntR].channel = CHANNEL; // pick a channel
+        slavesR[SlaveCntR].encrypt = 0; // no encryption
+        SlaveCntR++;
+      }
+    }
+  }
+
+  if (SlaveCntL > 0 || SlaveCntR > 0 ) {
+    Serial.print(SlaveCntL); Serial.println(" Left Slave(s) found, processing..");Serial.print(SlaveCntR); Serial.println(" Right Slave(s) found, processing..");
+  } else {
+    Serial.println("No Slave Found, trying again.");
+  }
+
+  // clean up ram
+  WiFi.scanDelete();
+}
+
+void printErrorESP(esp_err_t result) {
+  Serial.print("Send Status: ");
+  if (result == ESP_OK) {
+      Serial.println("Success");
+    } else if (result == ESP_ERR_ESPNOW_NOT_INIT) {
+      // How did we get so far!!
+      Serial.println("ESPNOW not Init.");
+    } else if (result == ESP_ERR_ESPNOW_ARG) {
+      Serial.println("Invalid Argument");
+    } else if (result == ESP_ERR_ESPNOW_INTERNAL) {
+      Serial.println("Internal Error");
+    } else if (result == ESP_ERR_ESPNOW_NO_MEM) {
+      Serial.println("ESP_ERR_ESPNOW_NO_MEM");
+    } else if (result == ESP_ERR_ESPNOW_NOT_FOUND) {
+      Serial.println("Peer not found.");
+    } else if (result == ESP_ERR_ESPNOW_FULL) {
+      Serial.println("Peer list full");
+    } else if (result == ESP_ERR_ESPNOW_NO_MEM) {
+      Serial.println("Out of memory");
+    } else if (result == ESP_ERR_ESPNOW_EXIST) {
+    Serial.println("Peer Exists");
+    } else {
+    Serial.println("Not sure what happened");
+    }
+}
+
+// Check if the slave is already paired with the master.
+// If not, pair the slave with master
+void manageSlave() {
+  if (SlaveCntL > 0) {
+    for (int i = 0; i < SlaveCntL; i++) {
+      Serial.print("Processing: ");
+      for (int ii = 0; ii < 6; ++ii ) {
+        Serial.print((uint8_t) slavesL[i].peer_addr[ii], HEX);
+        if (ii != 5) Serial.print(":");
+      }
+      Serial.print(" Status: ");
+      // check if the peer exists
+      bool exists = esp_now_is_peer_exist(slavesL[i].peer_addr);
+      if (exists) {
+        // Slave already paired.
+        Serial.println("Already Paired");
+      } else {
+        // Slave not paired, attempt pair
+        esp_err_t result = esp_now_add_peer(&slavesL[i]);
+        printErrorESP(result);
+      }
+    }
+  }
+  if (SlaveCntR > 0) {
+    for (int i = 0; i < SlaveCntR; i++) {
+      Serial.print("Processing: ");
+      for (int ii = 0; ii < 6; ++ii ) {
+        Serial.print((uint8_t) slavesR[i].peer_addr[ii], HEX);
+        if (ii != 5) Serial.print(":");
+      }
+      Serial.print(" Status: ");
+      // check if the peer exists
+      bool exists = esp_now_is_peer_exist(slavesR[i].peer_addr);
+      if (exists) {
+        // Slave already paired.
+        Serial.println("Already Paired");
+      } else {
+        // Slave not paired, attempt pair
+        esp_err_t result = esp_now_add_peer(&slavesR[i]);
+        printErrorESP(result);
+      }
+    }
+  } else {
+    // No slave found to process
+    Serial.println("No Slave found to process");
+  }
+}
+
+
+void brodcast(){
+  for (int i = 0; i < SlaveCntL; i++) {
+    const uint8_t *peer_addr = slavesL[i].peer_addr;
+    if (i == 0) { // print only for first slave
+      Serial.print("Sending: ");
+      Serial.println(data);
+    }
+    esp_err_t result = esp_now_send(peer_addr, &data, sizeof(data));
+    printErrorESP(result);
+  }
+  for (int i = 0; i < SlaveCntR; i++) {
+    const uint8_t *peer_addr = slavesR[i].peer_addr;
+    if (i == 0) { // print only for first slave
+      Serial.print("Sending: ");
+      Serial.println(data);
+    }
+    esp_err_t result = esp_now_send(peer_addr, &data, sizeof(data));
+    printErrorESP(result);
+  }
+}
+
+// send data
+void sendData() {
+  if (data%2){
+    int i = random(0,SlaveCntL);
+    const uint8_t *peer_addr = slavesL[i].peer_addr;
+    Serial.println(slavesL[i].peer_addr[5]);
+    Serial.print("Sending: ");
+    Serial.println(data);
+    esp_err_t result = esp_now_send(peer_addr, &data, sizeof(data));
+    printErrorESP(result);
+  }else {
+    int i = random(0,SlaveCntR);
+    const uint8_t *peer_addr = slavesR[i].peer_addr;
+    Serial.println(slavesR[i].peer_addr[5]);
+    Serial.print("Sending: ");
+    Serial.println(data);
+    esp_err_t result = esp_now_send(peer_addr, &data, sizeof(data));
+    printErrorESP(result);
+  }
+}
+
+void setup() {
+  pinMode(LaserPin, INPUT_PULLUP);
+  pinMode(LaserPin_1, INPUT_PULLUP);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  Serial.println("ESPNow/Multi-Slave/Master Example");
+  Serial.print("STA MAC: "); Serial.println(WiFi.macAddress());
+  InitESPNow();
+  ScanForSlave();
+}
+
+void checkLaser() {
+  // Lichtschranke durchbrochen
+  buttonState = digitalRead(LaserPin);
+  if (buttonState == HIGH){
+  } else {
+    if (SlaveCntL > 0 || SlaveCntR > 0 ) {
+      manageSlave();
+      // Stop
+      data = 0;
+      brodcast();
+      // Send new Song
+      data = random(1, maxFiles);
+      sendData();
+      buttonState = digitalRead(LaserPin);
+      while (buttonState == LOW) {
+        delay(10);
+        buttonState = digitalRead(LaserPin);
+      }
+      // debounce
+      delay(debounceValue);
+    }
+  }
+}
+
+void loop() {
+  checkLaser();
+  // Check EingangAusgang
+  buttonState = digitalRead(LaserPin_1);
+  if (buttonState == HIGH){
+  } else {
+    besucherzahler++;
+    if (SlaveCntL > 0 || SlaveCntR > 0 ) {
+      manageSlave();
+      data = 0;
+      brodcast();
+      if (besucherzahler%2) {
+        data = halloFile;
+      } else {
+        data = random(1, maxFiles);
+      }
+      sendData();
+      buttonState = digitalRead(LaserPin);
+      while (buttonState == LOW) {
+        delay(10);
+        buttonState = digitalRead(LaserPin);
+      }
+      // debounce
+      delay(debounceValue);
+    }
+  } 
+}
---
+++ SlavePlayer/SlavePlayer.ino
@@ -0,0 +1,89 @@
+#include "Arduino.h"
+#include <esp_now.h>
+#include <WiFi.h>
+#include <DFRobotDFPlayerMini.h>
+#define RXD2 16
+#define TXD2 17
+
+#define CHANNEL 0
+
+String Prefix = "Right:";//"Right:";
+
+DFRobotDFPlayerMini myDFPlayer;
+
+// Init ESP Now with fallback
+void InitESPNow() {
+  WiFi.disconnect();
+  if (esp_now_init() == ESP_OK) {
+    Serial.println("ESPNow Init Success");
+  }
+  else {
+    Serial.println("ESPNow Init Failed");
+    // Retry InitESPNow, add a counte and then restart?
+    // InitESPNow();
+    // or Simply Restart
+    ESP.restart();
+  }
+}
+
+// config AP SSID
+void configDeviceAP() {
+  String Mac = WiFi.macAddress();
+  String SSID = Prefix + Mac;
+  String Password = "123456789";
+  bool result = WiFi.softAP(SSID.c_str(), Password.c_str(), CHANNEL, 0);
+  if (!result) {
+    Serial.println("AP Config failed.");
+  } else {
+    Serial.println("AP Config Success. Broadcasting with AP: " + String(SSID));
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  Serial.println("ESPNow/Basic/Slave Example");
+  //Set device in AP mode to begin with
+  WiFi.mode(WIFI_AP);
+  // configure device AP mode
+  configDeviceAP();
+  // This is the mac address of the Slave in AP Mode
+  Serial.print("AP MAC: "); Serial.println(WiFi.softAPmacAddress());
+  // Init ESPNow with a fallback logic
+  InitESPNow();
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info.
+  esp_now_register_recv_cb(OnDataRecv);
+  if (!myDFPlayer.begin(Serial2)) {  //Use softwareSerial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while(true);
+  }
+   myDFPlayer.setTimeOut(500); //Set serial communictaion time out 500ms
+  
+  //----Set volume----
+  myDFPlayer.volume(10);  //Set volume value (0~30).
+  //myDFPlayer.volumeUp(); //Volume Up
+  //myDFPlayer.volumeDown(); //Volume Down
+  //myDFPlayer.sleep();
+}
+
+// callback when data is recv from Master
+void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print("Last Packet Recv from: "); Serial.println(macStr);
+  Serial.print("Last Packet Recv Data: "); Serial.println(*data);
+  Serial.println("");
+  if (*data== 0) {
+    myDFPlayer.pause();
+  }else {
+    myDFPlayer.play(*data);
+  }
+}
+
+void loop() {
+  // Chill
+}
